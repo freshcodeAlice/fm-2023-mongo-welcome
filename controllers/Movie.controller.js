c4fa798afee766af9111f925fd1201fa68f4b49b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-06-23 11:50:56 +0300

    search by query params

--- controllers/Movie.controller.js
+++ controllers/Movie.controller.js
@@ -33,3 +33,69 @@ module.exports.getOne = async (req, res, next) => {
         next(error);
     }
 }
+
+/*
+Завдання: дописати роути + методи контроллера на видалення та апдейт сутності.
+Для видалення спробуйте два методи: 
+findByIdAndDelete()
+findByIdAndRemove()
+і знайдіть різницю
+
+
+*/
+
+
+module.exports.updateOne = async (req, res, next) => {
+    try {
+        const {params: {id}, body} = req;
+        const updated = await Movie.findByIdAndUpdate(id, body, {new: true});
+        res.status(200).send({data: updated});
+    }catch(error) {
+        next(error);
+    }
+}
+
+
+module.exports.deleteOne = async (req, res, next) => {
+    try {
+        const {params: {id}} = req;
+        const deleted = await Movie.findByIdAndDelete(id);
+        console.log(deleted);
+        res.status(200).send({deleted});
+    }catch(error) {
+        next(error);
+    }
+}
+
+
+module.exports.getSearchOne = async (req, res, next) => {
+    try {
+        const {query} = req;
+        console.log(query);
+        const movie = await Movie.findOne({
+            ...query
+        });
+        res.status(200).send({data: movie})
+    } catch(error) {
+        next(error)
+    }
+}
+
+/*
+ Знайти фільм за параметрами запиту
+
+*/
+
+
+module.exports.getSearchAll = async (req, res, next) => {
+    try {
+        const {query} = req;
+        console.log(query);
+        const movie = await Movie.find({
+            ...query
+        });
+        res.status(200).send({data: movie})
+    } catch(error) {
+        next(error)
+    }
+}
--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -5,6 +5,10 @@ const movieRouter = express.Router();
 
 movieRouter.post('/', MovieController.createOne); //createOne
 movieRouter.get('/', MovieController.getAll); //getAll
-movieRouter.get('/:id', MovieController.getOne);
+movieRouter.get('/id/:id', MovieController.getOne);
+movieRouter.put('/:id', MovieController.updateOne);
+movieRouter.delete('/:id', MovieController.deleteOne);
+movieRouter.get('/search/', MovieController.getSearchOne); /// ?name=Taxi Driver
+movieRouter.get('/search/all/', MovieController.getSearchAll); /// ?name=Taxi Driver
 
 module.exports = movieRouter;
